b17720df654e28bb2b5544bdbcecfbb74c2501a3
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2016-12-30 08:56:43 -0800

    ends lecture 5

--- lecture5/app.js
+++ lecture5/app.js
@@ -2,7 +2,11 @@
   'use strict';
 
   angular.module('myFirstApp', [])
-  .controller('MyFirstController', function () {
 
+  .controller('MyFirstController', function ($scope) {
+    $scope.name = 'Rodrigo';
+    $scope.sayHello = function () {
+      return 'Hello ' + $scope.name;
+    };
   });
 })();
