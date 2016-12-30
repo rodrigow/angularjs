bd15da36b86e4f26a85f7f876d791fe5a93ebb15
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2016-12-30 14:10:46 -0800

    adds color to message

--- assignments/assignment1/assets/javascript/app.js
+++ assignments/assignment1/assets/javascript/app.js
@@ -8,11 +8,13 @@
   function LunchCheckController ($scope) {
     $scope.message = '';
     $scope.dishes = '';
+    $scope.paint = '';
 
     $scope.checkDishes = function () {
       if ($scope.dishes) {
         var dishList = $scope.dishes.split(',');
         if (dishList) {
+          $scope.paint = 'text-success';
           if (dishList.length > 3) {
             $scope.setMessage('Too much!');
           } else {
@@ -20,6 +22,7 @@
           }
         }
       } else {
+        $scope.paint = 'text-danger';
         $scope.setMessage('Please enter data first');
       }
     }
