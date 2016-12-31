2e8ae309770a30d9316fd8033ec92a2c79fcb208
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2016-12-30 23:08:58 -0800

    tests color option

--- assignments/assignment1/assets/javascript/appSpec.js
+++ assignments/assignment1/assets/javascript/appSpec.js
@@ -8,28 +8,65 @@ describe('Assignment 1 - LunchCheckController', function() {
   }));
 
   describe('$scope.checkDishes', function() {
+    var $scope, controller;
+
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('LunchCheckController', { $scope: $scope });
+    });
+
     it('tells users to Please enter data first', function () {
-      var $scope = {};
-      var controller = $controller('LunchCheckController', { $scope: $scope });
       $scope.dishes = '';
       $scope.checkDishes();
       expect($scope.message).toEqual('Please enter data first');
     });
 
     it("tells user to Enjoy!", function () {
-      var $scope = {};
-      var controller = $controller('LunchCheckController', { $scope: $scope });
       $scope.dishes = 'salad, water';
       $scope.checkDishes();
       expect($scope.message).toEqual('Enjoy!');
     });
 
     it("tells user that they ate Too Much!", function () {
-      var $scope = {};
-      var controller = $controller('LunchCheckController', { $scope: $scope });
       $scope.dishes = 'cake, soda, fries, cigar';
       $scope.checkDishes();
       expect($scope.message).toEqual('Too Much!');
+    });
+
+    it("shows the user a green color when one input is entered", function () {
+      $scope.dishes = 'bread';
+      $scope.checkDishes();
+      expect($scope.paint).toEqual('text-success');
+    });
+
+    it("shows the user a green color when four inputs are entered", function () {
+      $scope.dishes = 'bread, eggs, bacon, milk';
+      $scope.checkDishes();
+      expect($scope.paint).toEqual('text-success');
+    });
+
+    it("shows the user a red color when no input is entered", function () {
+      $scope.dishes = '';
+      $scope.checkDishes();
+      expect($scope.paint).toEqual('text-danger');
+    });
+
+    xit("eliminates empty options", function () {
+      $scope.dishes = ',';
+      $scope.checkDishes();
+      expect($scope.message).toEqual('Please enter data first');
+    });
+
+    xit("shows correct message with first option empty", function () {
+      $scope.dishes = ',bread,butter,jam';
+      $scope.checkDishes();
+      expect($scope.message).toEqual('Enjoy!');
+    });
+
+    xit("shows correct message with last option empty", function () {
+      $scope.dishes = 'bread,butter,jam,';
+      $scope.checkDishes();
+      expect($scope.message).toEqual('Enjoy!');
     });
 
   });
