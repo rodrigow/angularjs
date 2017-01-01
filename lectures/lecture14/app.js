961401ea86d7cde3d677d97dcb213e432d57682a
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2017-01-01 11:22:34 -0800

    ends lecture 14 - adding watchers manually

--- lectures/lecture14/app.js
+++ lectures/lecture14/app.js
@@ -4,13 +4,32 @@
   angular.module('CounterApp', [])
     .controller('CounterController', CounterController);
 
-    CounterController.$inject = ['$scope'];
-    function CounterController($scope) {
+  CounterController.$inject = ['$scope'];
+  function CounterController($scope) {
+    $scope.onceCounter = 0;
+    $scope.counter = 0;
 
-      $scope.showNumberOfWatchers = function () {
-
+    $scope.showNumberOfWatchers = function () {
+      console.log('watchersCount: ', $scope.$$watchersCount);
     };
 
+    $scope.countOnce = function () {
+      $scope.onceCounter = 1;
+    };
+
+    $scope.upCounter = function () {
+      $scope.counter++;
+    };
+
+    $scope.$watch('onceCounter', function(newValue, oldValue) {
+      console.log('onceCounter oldValue', oldValue);
+      console.log('onceCounter newValue', newValue);
+    });
+
+    $scope.$watch('counter', function(newValue, oldValue) {
+      console.log('counter oldValue', oldValue);
+      console.log('counter newValue', newValue);
+    });
   }
 
 })();
